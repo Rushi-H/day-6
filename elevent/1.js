0a9f0bd4225296ea5627f44f42a397d0df6968a6
Rushi-H <Rushihonkande@gmail.com>
2025-11-09 12:53:59 +0530

    add

---
+++ elevent/1.js
@@ -0,0 +1,5 @@
+rushi('rohit','pune');
+
+function rushi(name,city) {
+    console.log(`hello ${name} , welcome to ${city}`);
+}   
---
+++ elevent/main.js
@@ -0,0 +1,37 @@
+// const heading = document.querySelector('#appa');
+// console.log(heading.textContent);
+
+
+// heading1.textContent = 'New Heading Text';
+// heading1.style.color = 'blue';
+
+// const botton1 = document.getElementById('btn');
+// let clicked =   false;
+// botton1.addEventListener("click",()=>{
+//     if(!clicked){
+//         heading1.textContent = 'You Clicked the Button';
+//         heading1.style.color = 'green';
+        
+//         clicked = true;
+//     } else{
+//         heading1.textContent = 'You Clicked Again the Button';
+//         heading1.style.color = 'red';
+//         clicked = false;
+//     }
+// });
+
+
+
+// botton1.addEventListener('click',()=>{
+//     heading1.textContent = 'You Clicked the first timeButton';
+//     heading1.style.color = 'green';
+// });
+
+// const botton2 = document.getElementById('btn');
+// botton1.addEventListener('click',()=>{
+//     heading1.textContent = 'You Clicked 2nd time the Button';
+//     heading1.style.color = 'red';
+// });
+
+
+
