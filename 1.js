af8e3bfc5f4d9182a32dc66b0c65ab104bf7438f
Rushi-H <Rushihonkande@gmail.com>
2025-11-09 13:05:14 +0530

    23rd

---
+++ 1.js
@@ -0,0 +1,5 @@
+const name = "Alice";
+const age = 20;
+// console.log("My name is " + name + " and I am " + age + " years old.");
+console.log(`My name is ${name} and I am ${age} years old.`);
+console.log(`next year my age will be ${age +1}`);
---
+++ practice/main.js
@@ -0,0 +1,4 @@
+const fruits = ['apple', 'banana', ];
+const moreFruits = [...fruits,'orange', 'grape'];
+console.log(fruits); // Output: ['apple', 'banana']
+console.log(moreFruits); // Output: ['apple', 'banana', 'orange', 'grape']
